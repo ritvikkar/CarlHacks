0068d67fbe84fbdc49b815c8dcfe2c260a5ba266
Ritvik Kar <karr@carleton.edu>
2017-04-15 07:02:57 -0500

    Create a Table View

--- FoodTracker/FoodTracker/AppDelegate.swift
+++ FoodTracker/FoodTracker/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  FoodTracker
 //
-//  Created by Jane Appleseed on 10/17/16.
-//  Copyright © 2016 Apple Inc. All rights reserved.
+//  Created by Ritvik Kar on 04/15/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
 //
 
 import UIKit
