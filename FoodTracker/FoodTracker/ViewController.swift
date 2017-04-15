fb90abb9cc9afcfb8f0011b457bf8642d31b1eeb
Ritvik Kar <karr@carleton.edu>
2017-04-15 12:15:41 -0500

    Trying UI stuff

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ritvik Kar on 4/15/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 250/255, green: 160/255, blue: 0/255, alpha: 1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
